7b334b10f704d65e35a92d845ad33b84c03ecb99
= <=>
2021-12-01 21:51:42 -0300

    projeto iniciado

---
+++ src/main/java/com/projetofinal/sistemabibliotecario/SistemabibliotecarioApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinal.sistemabibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabibliotecarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemabibliotecarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetofinal/sistemabibliotecario/SistemabibliotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofinal.sistemabibliotecario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemabibliotecarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
